be5304c884d128c9f4afa20df551e616c1b5196c
yoons <alohalo98@naver.com>
2022-02-16 01:21:06 +0900

    moment 객체 이용하여 state 생성, header에 state를 format에 맞춰 props로 전달

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import Calendar from './Calendar';
 import Header from './Header';
-function App() {
-    return (
-        <div className="layout">
-            <div className="app-container">
-                <Header />
-                <Calendar />
+import moment from 'moment';
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {
+        YearMonth: moment(),
+        today: moment(),
+    };
+
+    render() {
+        return (
+            <div className="layout">
+                <div className="app-container">
+                    <Header
+                        YearMonth={this.state.YearMonth.format('MMMM YYYY년')}
+                        today={this.state.today.format('현재 YYYY - MM - DD')}
+                    />
+                    <Calendar />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
