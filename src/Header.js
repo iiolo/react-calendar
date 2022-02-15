bd46bdcb6c739e8079699bc48878e7c37be88438
yoons <alohalo98@naver.com>
2022-02-16 01:21:31 +0900

    props 전달 내용 확인

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { CalendarContainer } from 'react-datepicker';
 
 class Header extends Component {
     render() {
+        console.log(this.props);
         return <div className="header-container"></div>;
     }
 }
