ab00b6eef7e44b619f0ff8a9368c25ac67d6f24a
yoons <alohalo98@naver.com>
2022-02-16 00:34:43 +0900

    header, calendar 영역 나누기

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { CalendarContainer } from 'react-datepicker';
+
+class Header extends Component {
+    render() {
+        return <div className="header-container"></div>;
+    }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Calendar from './Calendar';
-
+import App from './App';
 ReactDOM.render(
     <React.StrictMode>
-        <Calendar />
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
