47d82becc0038dc8d43d7f8b96b1b68fddb1211c
yoons <alohalo98@naver.com>
2022-02-16 21:52:43 +0900

    양쪽 화살표 아이콘 클릭할 때마다 nextMonth 함수 활용하여 월 하나씩 증감 - left : -1 , right : +1

--- src/Header.js
+++ src/Header.js
@@ -12,10 +12,20 @@ class Header extends Component {
                 </h3>
                 <ul className="header-buttons header-middle">
                     <li>
-                        <i className="move-button arrow-left icon"></i>
+                        <i
+                            className="move-button arrow-left icon"
+                            onClick={() => {
+                                this.props.nextMonth(-1);
+                            }}
+                        ></i>
                     </li>
                     <li>
-                        <i className="move-button arrow-right icon"></i>
+                        <i
+                            className="move-button arrow-right icon"
+                            onClick={() => {
+                                this.props.nextMonth(+1);
+                            }}
+                        ></i>
                     </li>
                 </ul>
             </div>
